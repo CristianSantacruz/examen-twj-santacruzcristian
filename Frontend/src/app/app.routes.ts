dcf4627847978f72a7c47e47ba381eae5c25be49
CristianSantacruz <santacruz-guarquila@hotmail.com>
2017-03-06 09:48:22 -0500

    Routes

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import {Routes, RouterModule} from "@angular/router";
+import {ModuleWithProviders} from "@angular/core";
+import {HomeComponent} from "./home/home.component";
+import {EquipoComponent} from "./equipo/equipo.component";
+import {JugadorComponent} from "./jugador/jugador.component";
+/**
+ * Created by Belen on 05/03/2017.
+ */
+export const routes:Routes =[
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'equipo', component: EquipoComponent},
+  {path:'equipo/:idEquipo/jugador', component: JugadorComponent}
+];
+export const routing:ModuleWithProviders=RouterModule.forRoot(routes);
+

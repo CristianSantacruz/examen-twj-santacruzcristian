81e77e79e8f9d16e91c58b3474e049fc40955c49
CristianSantacruz <santacruz-guarquila@hotmail.com>
2017-03-07 01:23:22 -0500

    HTML

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,12 +3,11 @@ import {ModuleWithProviders} from "@angular/core";
 import {HomeComponent} from "./home/home.component";
 import {CelularComponent} from "./celular/celular.component";
 import {AplicacionComponent} from "./aplicacion/aplicacion.component";
-
 export const routes:Routes =[
-  {path: '', redirectTo:'home', pathMatch:'full'},
-  {path:'home', component: HomeComponent},
-  {path:'celulares', component: CelularComponent},
-  {path:'celulares/:idCelular/aplicacion', component: AplicacionComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'celular', component: CelularComponent},
+  {path: 'celular/:idCelular/aplicacion', component: AplicacionComponent}
 ];
 export const routing:ModuleWithProviders=RouterModule.forRoot(routes);
 
